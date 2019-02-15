6d54cc8a1f625623e1bb1366b867753767ba0f67
markhmwong <markhmwong@gmail.com>
2019-02-15 15:14:18 +1100

    Initial Commit

---
+++ ToThePi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToThePi
+//
+//  Created by Mark Wong on 15/2/19.
+//  Copyright © 2019 Mark Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ToThePiTests/ToThePiTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToThePiTests.swift
+//  ToThePiTests
+//
+//  Created by Mark Wong on 15/2/19.
+//  Copyright © 2019 Mark Wong. All rights reserved.
+//
+
+import XCTest
+@testable import ToThePi
+
+class ToThePiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
